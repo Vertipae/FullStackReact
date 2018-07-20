f3f712927456014ca69382cad02da6fddba6801b
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-07-20 18:33:51 +0300

    Metodien käyttö ja this

--- osa1/esimerkkisovellus1/src/index.js
+++ osa1/esimerkkisovellus1/src/index.js
@@ -2,60 +2,122 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-
-
-
+// Bindi konstruktorissa class properties -ominaisuudella
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
             counter: 1
         }
+        
+        this.kasvataYhdella = this.kasvataYhdella.bind(this)
+        this.nollaa = this.nollaa.bind(this)
     }
 
+    kasvataYhdella = () => {
+        this.setState({ counter: this.state.counter + 1})
+    }
+    // kasvataYhdella() {
+    //     this.setState({ counter: this.state.counter + 1 })
+    // }
+
+    nollaa = () => {
+        this.setState({ counter: 0})
+    }
+
+    // nollaa() {
+    //     this.setState({counter: 0})
+    // }
     render() {
         return (
             <div>
                 <div>{this.state.counter}</div>
-                {/* Jokainen napin painallus tulostaa konsoliin clicked */}
-                {/* <button onClick={() => console.log('clicked')}> */}
-
-                {/* Jokainen napin painallus kasvattaa laskuria */}
-                <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>
-                    plus
+                <div>
+                    {/* Viitataan metodeihin ilman bindiä */}
+                    <button onClick={this.kasvataYhdella}>
+                        plus
                 </button>
-                {/* Napin painallus nollaa laskurin */}
-                <button onClick={() => this.setState({ counter: 0 })}>
-                    zero
+                    <button onClick={this.nollaa}>
+                        nollaa
                 </button>
+                </div>
             </div>
         )
     }
 }
 
 
+
+// Bindin tekeminen tapa 1
+
 // class App extends React.Component {
-// constructor(props) {
-// super(props)
-// Tila sisältää kentän counter, jonka arvo on 1. Ei tulisi päivittää suoraan.
+//     constructor() {
+//         super()
+//         this.state = {
+//             counter: 1
+//         }
+//     }
+
+//     kasvataYhdella() {
+//         this.setState({ counter: this.state.counter + 1 })
+//     }
+
+//     nollaa() {
+//         this.setState({ counter: 0 })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div>{this.state.counter}</div>
+//                 <div>
+//                     {/* Ei toimi this näin koska this ei enää viittaa komponenttiin App vaan on arvoltaan undefined. Virhe korjataan käyttäen bindiä */}
+//                     {/* <button onClick={this.kasvataYhdella}> */}
+
+//                     {/* Bindaamisella muodostetaan uusi funktio, jonka koodi on alkuperäisen funktion koodi */}
+//                     <button onClick={this.kasvataYhdella.bind(this)}>
+//                         plus
+//                     </button>
+//                     <button onClick={this.nollaa.bind(this)}>
+//                         zero
+//                     </button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+// class App extends React.Component {
+// constructor() {
+// super()
 // this.state = {
 // counter: 1
 // }
-
-// setInterval(() => {
-// Tilan päivitys tapahtuu setState funktion avulla. Kutsutaan funktiota setState toistuvasti sekunnin välein korottaen laskurin arvoa aina yhdellä.
-// this.setState({ counter: this.state.counter + 1})
-// }, 1000)
 // }
 
 // render() {
-// Konsolissa tulostetaan
-// console.log('Renderöidään', this.state.counter)
 // return (
+// <div>
 // <div>{this.state.counter}</div>
+// {/* Jokainen napin painallus tulostaa konsoliin clicked */ }
+// {/* <button onClick={() => console.log('clicked')}> */ }
+
+// {/* Jokainen napin painallus kasvattaa laskuria */ }
+// <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>
+// plus
+// </button>
+// {/* Napin painallus nollaa laskurin */ }
+// <button onClick={() => this.setState({ counter: 0 })}>
+// zero
+// </button>
+// </div>
 // )
 // }
 // }
+
+
 
 ReactDOM.render(
     <App />,
