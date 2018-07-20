f684c710ae385d2d186ce9b13f0686e7de8c5610
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-07-20 12:49:49 +0300

    Tapahtumankäsittely

--- osa1/esimerkkisovellus1/src/index.js
+++ osa1/esimerkkisovellus1/src/index.js
@@ -2,40 +2,60 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-// Hello on komponentti, komponentin määrittelevällä funktiolla on nyt parametri props. Komponentti kirjoitetaan isolla eli Hello.
-// const Hello = (props) => {
-    // return (
-        // <div>
-            // <p>Hello {props.name}, you are {props.age} years old</p>
-        // </div>
-    // )
-// }
 
-// Funktio on kaikki sulkujen sisässä nuolen jälkeen
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        // Tila sisältää kentän counter, jonka arvo on 1. Ei tulisi päivittää suoraan.
+    constructor() {
+        super()
         this.state = {
             counter: 1
         }
-
-        setInterval(() => {
-            // Tilan päivitys tapahtuu setState funktion avulla. Kutsutaan funktiota setState toistuvasti sekunnin välein korottaen laskurin arvoa aina yhdellä.
-            this.setState({ counter: this.state.counter + 1})
-        }, 1000)
     }
 
     render() {
-        // Konsolissa tulostetaan
-        console.log('Renderöidään', this.state.counter)
         return (
-            <div>{this.state.counter}</div>
+            <div>
+                <div>{this.state.counter}</div>
+                {/* Jokainen napin painallus tulostaa konsoliin clicked */}
+                {/* <button onClick={() => console.log('clicked')}> */}
+
+                {/* Jokainen napin painallus kasvattaa laskuria */}
+                <button onClick={() => this.setState({ counter: this.state.counter + 1 })}>
+                    plus
+                </button>
+                {/* Napin painallus nollaa laskurin */}
+                <button onClick={() => this.setState({ counter: 0 })}>
+                    zero
+                </button>
+            </div>
         )
     }
 }
+
+
+// class App extends React.Component {
+// constructor(props) {
+// super(props)
+// Tila sisältää kentän counter, jonka arvo on 1. Ei tulisi päivittää suoraan.
+// this.state = {
+// counter: 1
+// }
+
+// setInterval(() => {
+// Tilan päivitys tapahtuu setState funktion avulla. Kutsutaan funktiota setState toistuvasti sekunnin välein korottaen laskurin arvoa aina yhdellä.
+// this.setState({ counter: this.state.counter + 1})
+// }, 1000)
+// }
+
+// render() {
+// Konsolissa tulostetaan
+// console.log('Renderöidään', this.state.counter)
+// return (
+// <div>{this.state.counter}</div>
+// )
+// }
+// }
 
 ReactDOM.render(
     <App />,
@@ -43,31 +63,5 @@ ReactDOM.render(
 )
 
 
-// const App = (props) => {
-    // const { counter } = props
-
-    // return (
-        // <div>{counter.value}</div>
-    // )
-// };
-
-// const counter = {
-    // value: 1
-// }
-
-// const renderoi = () => {
-    // ReactDOM.render(<App counter={counter} />, document.getElementById('root'))
-// }
-
-// setInterval(() => {
-    // renderoi()
-    // counter.value += 1
-// },)
-
-// renderoi()
-// counter.value += 1
-// renderoi()
-// counter.value += 1
-// renderoi()
 
 
