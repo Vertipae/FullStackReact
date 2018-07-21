bce0a4306e9aafedfe345e846c74d647358f779d
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-07-21 20:45:58 +0300

    Funktio joka palauttaa funktion

--- osa1/esimerkkisovellus1/src/index.js
+++ osa1/esimerkkisovellus1/src/index.js
@@ -1,56 +1,83 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-// Bindi konstruktorissa class properties -ominaisuudella
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
             counter: 1
+
         }
-
-        this.kasvataYhdella = this.kasvataYhdella.bind(this)
-        this.nollaa = this.nollaa.bind(this)
     }
-    kasvataYhdella = () => {
-        this.setState((prevState) => ({
-            counter: prevState.counter + 1
-        }))
-    }
+    // Metodi sisältää vain yhden komennon, eli returnin niin hyödynnetään nuolifunktion tiiviimpää muotoa "kaksi nuolta sisältävä funktio"
+    asetaArvoon = (arvo) => () => this.setState({ counter: arvo })
 
+    // Pidempi muoto
+    // asetaArvoon = (arvo) => {
+    //     return () => {
+    //         this.setState({ counter: arvo })
+    //     }
 
-    // setState kutsut eivät välttämättä tapahdu siinä järjestyksessä missä ne on kirjoitettu koodiin
-    // kasvataYhdella = () => {
-    //     this.setState({ counter: this.state.counter + 1 })
     // }
-
-
-    nollaa = () => {
-        // React kutsuu funktiota setState asynkroonisesti, joten molemmat rivit tulostavat saman arvon sillä Reactin tila ei saa uutta arvoa heti komennon this.setState jälkeen
-        console.log(this.state.counter)
-        this.setState({ counter: 0 })
-        console.log(this.state.counter)
-    }
-
 
     render() {
         return (
             <div>
                 <div>{this.state.counter}</div>
                 <div>
-                    {/* Viitataan metodeihin ilman bindiä */}
-                    <button onClick={this.kasvataYhdella}>
+                    <button onClick={this.asetaArvoon(this.state.counter + 1)}>
                         plus
-                </button>
-                    <button onClick={this.nollaa}>
-                        nollaa
-                </button>
+                    </button>
+                    <button onClick={this.asetaArvoon(0)}>
+                        zero
+                    </button>
                 </div>
             </div>
         )
     }
+
 }
+
+
+// Bindi konstruktorissa class properties -ominaisuudella
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             counter: 1
+//         }
+
+//         this.kasvataYhdella = this.kasvataYhdella.bind(this)
+//         this.nollaa = this.nollaa.bind(this)
+//     }
+//     kasvataYhdella = () => {
+//         this.setState((prevState) => ({
+//             counter: prevState.counter + 1
+//         }))
+//     }
+
+//     nollaa = () => {
+//         this.setState({ counter: 0 })
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <div>{this.state.counter}</div>
+//                 <div>
+//                     {/* Viitataan metodeihin ilman bindiä */}
+//                     <button onClick={this.kasvataYhdella}>
+//                         plus
+//                 </button>
+//                     <button onClick={this.nollaa}>
+//                         nollaa
+//                 </button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 ReactDOM.render(
     <App />,
