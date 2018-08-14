a79d6f1264781c50e36c877485a21aaf7659bda2
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-08-15 00:10:33 +0300

    Resurssin poisto

--- osa3/backend/index.js
+++ osa3/backend/index.js
@@ -1,8 +1,4 @@
 
-
-// Ottaa käyttöön Noden sisäänrakennetun web-palvelimen määrittelevän moduulin. Sama asia kuin import http from 'http'
-
-
 // Heti alussa otetaan käyttöön express, joka on tällä kertaa funktio, jota kutsumalla luodaan muuttujaan app sijoitettava express-sovellusta vastaava olio
 const express = require('express')
 const app = express()
@@ -28,15 +24,13 @@ let notes = [
     }
 
 ]
-// Määritellään sovellukselle kaksi routea. Näistä ensimmäinen määrittelee tapahtumankäsittelijän, joka hoitaa sovelluksen juureen eli polkuun / tulevia HTTP GET -pyyntöjä
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
-// Routeista toinen määrittelee tapahtumankäsittelijän, joka hoitaa sovelluksen polkuun notes tulevia HTTP GET -pyyntöjä
-// Pyyntöön vastataan response-olion metodilla json, joka lähettää HTTP-pyynnön vastaukseksi parametrina olevaa Javascript-olioa (eli taulukkoa notes) vastaavan JSON-muotoisen merkkijonon. 
+
 app.get('/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    // const id = request.params.id
     // console.log(id)
     const note = notes.find(note => note.id === id)
     // console.log(note)
@@ -46,11 +40,14 @@ app.get('/notes/:id', (request, response) => {
         // Metodin status lisäksi metodia end ilmoittamaan siitä, että pyyntöön tulee vastata ilman dataa
         response.status(404).end()
     }
-    // const note = notes.find(note => {
-    // console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    // })
-    // console.log(note)
-    // response.json(note)
+
+})
+
+app.delete('/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
