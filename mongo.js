4de046d4a7839be3a0f6ce14e8b17cddbcd16c0a
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-08-20 18:00:46 +0300

    Olioiden hakeminen tietokannasta

--- mongo.js
+++ mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 // EI oo mun, esimerkki
 // const url = 'mongodb://fullstack:sekred@ds211088.mlab.com:11088/fullstack-notes'
-
+console.log(process.env.MONGODB_URI)
 const url = process.env.MONGODB_URI
 mongoose.connect(url)
 
@@ -14,6 +15,15 @@ const noteSchema = new mongoose.Schema({
 })
 
 const Note = mongoose.model('Note', noteSchema)
+// Oliot haetaan kannasta Note-modelin metodilla find. Metodin parametrina on hakuehto. Koska hakuehtona on tyhjä olio {}, saimme kannasta kaikki notes-kokoelmaan talletetut oliot.
+Note
+    .find({})
+    .then(result => {
+        result.forEach(note => {
+            console.log(note)
+        })
+        mongoose.connection.close()
+    })
 
 // const Note = mongoose.model('Note', {
 // content: String,
@@ -21,21 +31,21 @@ const Note = mongoose.model('Note', noteSchema)
 // important: Boolean
 // })
 
-const note = new Note({
-    content: 'HTML on helppoa',
-    date: new Date(),
-    important: true
-})
-// Sovellus luo muistiinpanoa vastaavan model:in avulla muistiinpano-olion
-const note = new Note({
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: new Date(),
-    important: false
-})
+// const note = new Note({
+//     content: 'HTML on helppoa',
+//     date: new Date(),
+//     important: true
+// })
+// // Sovellus luo muistiinpanoa vastaavan model:in avulla muistiinpano-olion
+// const note = new Note({
+//     content: 'Selain pystyy suorittamaan vain javascriptiä',
+//     date: new Date(),
+//     important: false
+// })
 
-note
-    .save()
-    .then(response => {
-        console.log('note saved')
-        mongoose.connection.close()
-    })
+// note
+//     .save()
+//     .then(response => {
+//         console.log('note saved')
+//         mongoose.connection.close()
+//     })
